3e02366a9a163d97a6089b2b52d1495485701c89
Mwakanemela <kayangemwakanemela@gmail.com>
2024-05-17 03:30:57 -0700

    for doc picker added: file type, file name file size and many more

--- app/src/main/java/com/example/practicaldemo/docpicker/DocPicker.kt
+++ app/src/main/java/com/example/practicaldemo/docpicker/DocPicker.kt
@@ -1,23 +1,34 @@
 package com.example.practicaldemo.docpicker
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.OpenableColumns
 import android.util.Log
+import android.webkit.MimeTypeMap
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.practicaldemo.R
+import com.tom_roush.pdfbox.pdmodel.PDDocument
+import java.io.InputStream
+import java.util.Locale
+import kotlin.math.log10
+import kotlin.math.pow
 
 class DocPicker : AppCompatActivity() {
     private lateinit var docPicker: Button
     private lateinit var imageView: ImageView
+    private lateinit var docTitle: TextView
+    private lateinit var docInfo: TextView
 
     private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
@@ -39,6 +50,8 @@ class DocPicker : AppCompatActivity() {
 
         docPicker = findViewById(R.id.button)
         imageView = findViewById(R.id.image)
+        docTitle = findViewById(R.id.docTitle)
+        docInfo = findViewById(R.id.docInfo)
 
         docPicker.setOnClickListener {
             openFilePicker()
@@ -48,28 +61,99 @@ class DocPicker : AppCompatActivity() {
     private fun openFilePicker() {
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
             addCategory(Intent.CATEGORY_OPENABLE)
-//            type = "application/*"
-            type = "*/*"
+            type = "application/*"
+//            type = "*/*"
 //            type = "images/*" // Set MIME type to select all types of documents
         }
         getContent.launch(intent)
     }
+    @SuppressLint("SetTextI18n")
     private fun handleDocumentUri(uri: Uri) {
         // Handle the selected document URI here
         // For example, you can retrieve the file name
         contentResolver.query(uri, null, null, null, null)?.use { cursor ->
             val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+            val sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
+
             cursor.moveToFirst()
             val fileName = cursor.getString(nameIndex)
+            val fileSize = cursor.getLong(sizeIndex)
+            docTitle.text = fileName
+//            val inputStream = assets.open(uri.toString())
+//            val document = PDDocument.load(inputStream)
+            val numberOfPages = getNumberOfPagesFromUri(this, uri)
 
+            val fileSizes = formatFileSize(fileSize)
+
+            val documentType = documentType(fileName)
+            docInfo.text = "$numberOfPages pages . $fileSizes . $documentType"
             Log.d("FileName", ": $fileName")
             Log.d("FileName", "uri $uri")
-            val filePath = FilePaths(this).getPathFromUri(uri)
-            Log.d("FileName", ":file path: $filePath")
+//            val filePath = FilePaths(this).getPathFromUri(uri)
+//            Log.d("FileName", ":file path: $filePath")
+
+//            document.close()
 
             // Now you have the file name, you can do further processing
             // such as displaying it in a TextView or performing operations on the file
         }
     }
+
+    private fun getNumberOfPagesFromUri(context: Context, uri: Uri): Int {
+        var inputStream: InputStream? = null
+        var numberOfPages = 0
+        try {
+            inputStream = context.contentResolver.openInputStream(uri)
+            if (inputStream != null) {
+                val document = PDDocument.load(inputStream)
+                numberOfPages = document.numberOfPages
+                document.close()
+            }
+        } catch (e: Exception) {
+            // Handle exceptions
+            e.printStackTrace()
+        } finally {
+            inputStream?.close()
+        }
+        return numberOfPages
+    }
+
+    @SuppressLint("DefaultLocale")
+    fun formatFileSize(sizeBytes: Long): String {
+        if (sizeBytes <= 0) return "0 B"
+
+        val units = arrayOf("B", "KB", "MB", "GB", "TB")
+        val digitGroups = (log10(sizeBytes.toDouble()) / log10(1024.0)).toInt()
+
+        return String.format("%.2f %s", sizeBytes / 1024.0.pow(digitGroups.toDouble()), units[digitGroups])
+    }
+
+//    fun getDocumentTypeFromUri(uri: Uri): String? {
+//        val extension = MimeTypeMap.getFileExtensionFromUrl(uri.toString())
+//        return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)
+//    }
+
+    private fun documentType(fileName: String) : String {
+        val extension = fileName.substringAfterLast(".", "Unknown")
+        return extension
+    }
+    private fun getDocumentTypeFromUri(uri: Uri): String {
+//        val extension = MimeTypeMap.getFileExtensionFromUrl(uri.toString()) ?: return "Unknown"
+        val fileName = uri.lastPathSegment ?: return "Unknown"
+        val extension = fileName.substringAfterLast(".", "Unknown")
+        return extension
+        // Manually map common file extensions to MIME types
+//        return when (extension.lowercase(Locale.getDefault())) {
+//            "pdf" -> "application/pdf"
+//            "doc", "docx" -> "application/msword"
+//            "xls", "xlsx" -> "application/vnd.ms-excel"
+//            "ppt", "pptx" -> "application/vnd.ms-powerpoint"
+//            "txt" -> "text/plain"
+//            // Add more mappings as needed
+//            else -> "Unknown"
+//        }
+    }
+
+
 }
 
---
+++ app/src/main/java/com/example/practicaldemo/workmanager/WorkManagerActivity.kt
@@ -0,0 +1,21 @@
+package com.example.practicaldemo.workmanager
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.practicaldemo.R
+
+class WorkManagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_work_manager)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
